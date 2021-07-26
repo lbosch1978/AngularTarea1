acaed2aec798fc67fa7468df93df458d494e975e
Luis Bosch <softnetgt@gmail.com>
2021-07-25 22:30:40 -0600

    Panel, Button, bullet - schedule

---
+++ src/app/controls/controls.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PanelComponent } from './panel/panel.component';
+import { ButtonComponent } from './button/button.component';
+import { BulletComponent } from './bullet/bullet.component';
+
+
+
+@NgModule({
+  declarations: [
+    PanelComponent,
+    ButtonComponent,
+    BulletComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ControlsModule { }
